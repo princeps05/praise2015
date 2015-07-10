dea8e10005c3049b6f6871a8ceba45e886d4d4b5
lee <princeps05@naver.com>
2015-07-11 00:31:03 +0900

    카카오톡 링크 추가

--- scripts/components/Info.jsx
+++ scripts/components/Info.jsx
@@ -2,12 +2,39 @@ var React = require('react');
 
 var Info = React.createClass({
 
+  componentWillMount: function() {
+    Kakao.cleanup();    
+  },
+
+  componentDidMount: function(prevProps, prevState) {
+
+    Kakao.init('14b6a997dfd278344236da1878d39837');
+    Kakao.Link.createTalkLinkButton({
+      container: React.findDOMNode(this.refs.kakao),
+      image: {
+        src: 'http://vespasiani.cdn3.cafe24.com/img/dure.jpg',
+        width: '300',
+        height: '200'
+      },
+      webButton: {
+        text: '두레찬양',
+        url: 'http://xn--9d0bt6kv7c355b.com/praise/'
+      }
+    });  
+  },
+
 	render: function() {
-		
+    
 	    return (
     		<div className="wrap panel panel-default">
-          <div className="panel-heading">두레찬양 3번째 버전 개발중</div>
+
           <div className="panel-body">
+
+            <ul className="list-group">
+              <a id="kakao-link-btn" ref="kakao"><img className="kakao" src="http://vespasiani.cdn3.cafe24.com/img/kakao.png" /></a>
+              <li className="list-group-item list-group-item-info">두레찬양 모바일 웹</li>
+            </ul>
+
             <ul className="list-group">
               <li className="list-group-item">악보에서 좌/우드래그로 이전/다음장 보기</li>
               <li className="list-group-item">악보에서 상단 오른쪽에 V터치해서 내역 저장</li>
@@ -17,7 +44,7 @@ var Info = React.createClass({
             </ul>
             
             <div className="list-group">
-              <a className="list-group-item list-group-item-success">github 소스코드 업로드</a>
+              <li className="list-group-item list-group-item-success">github 소스코드 업로드</li>
               <a href="https://github.com/princeps05/praise2012/" target="2012" className="list-group-item">https://github.com/princeps05/praise2012/</a>
               <a href="https://github.com/princeps05/praise2014/" target="2014" className="list-group-item">https://github.com/princeps05/praise2014/</a>
               <a href="https://github.com/princeps05/praise2015/" target="2015" className="list-group-item">https://github.com/princeps05/praise2015/</a>
